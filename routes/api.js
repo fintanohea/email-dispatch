d97d0bdea875c774771d2a29fc7dd05bc65125ed
Fintan O'Hea <fintanohea@gmail.com>
2020-03-03 12:10:35 -0800

    added send form and send email through post request

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,45 @@ var router = express.Router();
 
 const helper = require('@sendgrid/mail');
 
+router.post('/:action', function(req, res, next) {
+    var action = req.params.action;
+
+    if (action === 'send') {
+
+        const sgMail = require('@sendgrid/mail');
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+        const msg = {
+            to: req.body.recipient,
+            from: 'app163557149@heroku.com',
+            subject: req.body.subject,
+            text: req.body.content,
+            html: req.body.html,
+        };
+        
+        //ES8
+        (async () => {
+            try {
+                await sgMail.send(msg);
+                res.json({
+                    confirmation: 'success',
+                    message: 'Email was sent'
+                });
+            } catch (err) {
+                res.json({
+                    confirmation: 'fail',
+                    message: err.toString()
+                });
+            }
+        })();
+
+        return;
+    }
+
+    res.json({
+        confirmation: 'fail',
+        message: 'Invalid Action'
+    });
+});
 
 /* GET users listing. */
 router.get('/:action', function(req, res, next) {
