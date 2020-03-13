383d82be6136ac0cde13b9cbc86796eeed52cfb2
Fintan O'Hea <fintanohea@gmail.com>
2020-03-12 17:11:04 -0700

    cleanup

--- dummy/emails.js
+++
@@ -1,9 +0,0 @@
-const emails = [
-    {
-        recipients: 'testfrom@gmail.com',
-        subject: 'Test Subject',
-        content: 'Test content',
-        html: 'Test content'
-    }
- ];
- export default emails;
